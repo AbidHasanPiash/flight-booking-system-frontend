8d1e07479b7c87ad96c0b73348e0e2aa71f2f284
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-11-23 00:32:46 +0600

    flight search form added

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,32 +1,92 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { Link } from "react-router-dom";
+import React from "react";
+import Submit from "../components/buttons/Submit";
+import Spinner from "../components/common/Spinner";
+import InputWrapper from "../components/common/InputWrapper";
+import * as Yup from "yup";
+import { RiSendPlaneLine } from "react-icons/ri";
+import { toast } from "sonner";
+import { useFormWithMutation } from "../utils/useFormWithMutation";
 
 export default function SearchResults() {
-  // const [flights, setFlights] = useState([]);
 
-  // useEffect(() => {
-  //   // Fetch flights based on search criteria
-  //   const fetchFlights = async () => {
-  //     const response = await axios.get("/api/flights");
-  //     setFlights(response.data);
-  //   };
-  //   fetchFlights();
-  // }, []);
+    const validationSchema = Yup.object({
+        origin: Yup.string().required("Origin is required"),
+        destination: Yup.string().required("Destination is required"),
+        date: Yup.string().required("Date is required"),
+    });
 
-  return (
-    <div className="search-results">
-      <h2>Available Flights</h2>
-      {/* <ul>
-        {flights?.map((flight) => (
-          <li key={flight._id}>
-            <h3>{flight.airline}</h3>
-            <p>{flight.origin} to {flight.destination}</p>
-            <p>Price: ${flight.price}</p>
-            <Link to={`/flight/${flight._id}`}>View Details</Link>
-          </li>
-        ))}
-      </ul> */}
-    </div>
-  )
+    const initialValues = {
+        origin: "Iran",
+        destination: "Los Angeles",
+        date: "",
+    };
+
+    const onSubmit = async (data) => {
+        toast.success(JSON.stringify(data));
+    };
+
+    const onSuccess = () => {
+        formik.resetForm();
+    };
+
+    const { formik, mutation } = useFormWithMutation({
+        initialValues,
+        validationSchema,
+        onSubmit,
+        onSuccess,
+    });
+    return (
+        <div>
+            <h2>Available Flights</h2>
+
+            <div className="py-20">
+                <form onSubmit={formik.handleSubmit} className="max-w-7xl mx-auto bg-white p-2 rounded-lg shadow-lg" >
+
+                    <div className=" flex gap-4">
+                        <InputWrapper label="Origin" error={formik.errors?.origin} touched={formik.touched?.origin}>
+                            <input
+                                name="origin"
+                                placeholder="Origin"
+                                value={formik.values?.origin}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`w-full px-2 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400`}
+                            />
+                        </InputWrapper>
+
+                        <InputWrapper label="Destination" error={formik.errors?.destination} touched={formik.touched?.destination}>
+                            <input
+                                name="destination"
+                                placeholder="Destination"
+                                value={formik.values?.destination}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`w-full px-2 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400`}
+                            />
+                        </InputWrapper>
+
+                        <InputWrapper label="Date" error={formik.errors?.date} touched={formik.touched?.date}>
+                            <input
+                                name="date"
+                                type="date"
+                                value={formik.values?.date}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`w-full px-2 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400`}
+                            />
+                        </InputWrapper>
+                    </div>
+
+                    <div className="text-center">
+                        <Submit
+                            disabled={mutation.isPending}
+                            label={mutation.isPending ? "Searching..." : "Search"}
+                            icon={mutation.isPending ? <Spinner size="4" /> : <RiSendPlaneLine />}
+                            className="w-full"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }

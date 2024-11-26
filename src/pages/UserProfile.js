e7f46fe43d3c1ecfc1e778d646620ba1aff1e72c
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-11-26 14:43:08 +0600

    refactor: current booking card

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -26,11 +26,11 @@ export default function UserProfile() {
     };
 
     const currentBooking = bookings?.[bookings.length - 1]; // Display the latest booking
-    
+
 
     // Cancel mutation
     const cancelMutation = useMutation({
-        mutationFn: (id) => updateData(apiConfig?.UPDATE_BOOKING + id, {status: 'cancelled'}), // API endpoint for deletion
+        mutationFn: (id) => updateData(apiConfig?.UPDATE_BOOKING + id, { status: 'cancelled' }), // API endpoint for deletion
         onSuccess: () => {
             // Invalidate flights query to refresh the table
             queryClient.invalidateQueries(["flightBookings"]);
@@ -41,12 +41,25 @@ export default function UserProfile() {
     });
 
     // Delete flight handler
-    const onCancelBooking = (id) => {
-        if (window.confirm("Are you sure you want to Cancel this booking?")) {
+    const onCancelBooking = (id, departureDate) => {
+        const now = new Date(); // Current time
+        const departureTime = new Date(departureDate); // Convert departureDate to Date object
+
+        // Calculate the time difference in milliseconds
+        const timeDifference = departureTime - now;
+
+        // Check if the departure time is within 1 hour
+        if (timeDifference <= 60 * 60 * 1000) { // 1 hour in milliseconds
+            alert("Bookings can only be canceled at least 1 hour before the departure time.");
+            return;
+        }
+
+        // Proceed with cancellation if the condition is satisfied
+        if (window.confirm("Are you sure you want to cancel this booking?")) {
             cancelMutation.mutate(id);
         }
     };
-    
+
 
     if (isLoading) {
         return <div className="text-center mt-10">Loading...</div>;
@@ -54,65 +67,112 @@ export default function UserProfile() {
 
     return (
         <div className="min-h-screen bg-gray-50 py-12">
-            <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-6">
+            <div className="max-w-7xl mx-auto p-6">
                 {/* Header */}
-                <div className="flex justify-between items-center mb-6">
-                    <div>
-                        <h1 className="text-2xl font-bold text-gray-800">User Profile</h1>
-                        <Link to={'edit'} className="text-blue-600 hover:underline">
-                            Edit Profile
-                        </Link>
+                <div className="bg-white rounded-lg p-4">
+                    <div className="flex justify-between items-center mb-6">
+                        <div>
+                            <h1 className="text-2xl font-bold text-gray-800">User Profile</h1>
+                            <Link to={'edit'} className="text-blue-600 hover:underline">
+                                Edit Profile
+                            </Link>
+                        </div>
+                        <button
+                            onClick={handleLogout}
+                            className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 focus:ring-2 focus:ring-red-400 transition"
+                        >
+                            Logout
+                        </button>
                     </div>
-                    <button
-                        onClick={handleLogout}
-                        className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 focus:ring-2 focus:ring-red-400 transition"
-                    >
-                        Logout
-                    </button>
-                </div>
 
-                {/* User Information */}
-                <div className="mb-8">
-                    <h2 className="text-xl font-semibold text-gray-700">Personal Information</h2>
-                    <p className="text-gray-600">Name: {user.username}</p>
-                    <p className="text-gray-600">Email: {user.email}</p>
-                    <p className="text-gray-600">Role: {user.role}</p>
+                    {/* User Information */}
+                    <div className="mb-8">
+                        <h2 className="text-xl font-semibold text-gray-700">Personal Information</h2>
+                        <p className="text-gray-600">Name: {user.username}</p>
+                        <p className="text-gray-600">Email: {user.email}</p>
+                        <p className="text-gray-600">Role: {user.role}</p>
+                    </div>
                 </div>
 
                 {/* Current Booking */}
-                <div className="mb-8">
-                    <h2 className="text-xl font-semibold text-gray-700">Current Booking</h2>
+                <div className="my-8">
+                    <h2 className="text-xl font-semibold text-gray-700 mb-4">Current Booking</h2>
                     {currentBooking ? (
-                        <div className="bg-blue-50 p-4 rounded-lg border border-blue-200">
+                        <div className="bg-gradient-to-br from-blue-100 via-white to-blue-50 shadow-md rounded-xl border border-blue-300 p-6">
                             <div className="flex items-start justify-between">
                                 <div className="flex items-center gap-4 mb-4">
-                                    <FaPlaneDeparture className="text-blue-600 text-2xl" />
+                                    <FaPlaneDeparture className="text-blue-500 text-3xl" />
                                     <div>
-                                        <p className="text-gray-700 font-medium">Flight: {currentBooking.flightId}</p>
-                                        <p className="text-gray-600">Seats: {currentBooking.numberOfSeats}</p>
+                                        <p className="text-lg text-gray-800 font-bold">
+                                            Flight: {currentBooking?.flightDetails?.flightNumber}
+                                        </p>
+                                        <p className="text-gray-600 font-medium">
+                                            Seats: {currentBooking.numberOfSeats}
+                                        </p>
                                     </div>
                                 </div>
                                 <button
-                                    onClick={() => onCancelBooking(currentBooking?._id)}
+                                    onClick={() => onCancelBooking(currentBooking?._id, currentBooking?.flightDetails?.departureDate)}
                                     disabled={currentBooking?.status === 'cancelled'}
-                                    className="text-red-500 hover:bg-red-100 px-2 py-1 rounded-lg disabled:cursor-not-allowed">
-                                    Cancel
+                                    className="px-4 py-2 bg-red-500 text-white font-medium rounded-md shadow-md hover:bg-red-600 focus:ring-2 focus:ring-red-300 transition disabled:bg-gray-300 disabled:cursor-not-allowed"
+                                >
+                                    Cancel Booking
                                 </button>
                             </div>
-                            <p className="text-gray-600">
-                                <strong>Date:</strong> {new Date(currentBooking.bookingDate).toLocaleDateString()}
-                            </p>
-                            <p className="text-gray-600">
-                                <strong>Total Price:</strong> ${currentBooking.totalPrice.toFixed(2)}
-                            </p>
-                            <p className="text-gray-600">
-                                <strong>Status:</strong> {currentBooking.status}
-                            </p>
+
+                            <div className="mt-4">
+                                <p className="flex items-center text-gray-700 text-sm gap-2">
+                                    <strong className="text-gray-800">Airline:</strong> {currentBooking.flightDetails?.airline}
+                                </p>
+                                <p className="flex items-center text-gray-700 text-sm gap-2">
+                                    <strong className="text-gray-800">Origin:</strong> {currentBooking.flightDetails?.origin}
+                                </p>
+                                <p className="flex items-center text-gray-700 text-sm gap-2">
+                                    <strong className="text-gray-800">Destination:</strong> {currentBooking.flightDetails?.destination}
+                                </p>
+                                <p className="flex items-center text-gray-700 text-sm gap-2">
+                                    <strong className="text-gray-800">Booking Date:</strong>
+                                    {new Date(currentBooking?.bookingDate).toLocaleString("en-US", {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric",
+                                        hour: "2-digit",
+                                        minute: "2-digit",
+                                        hour12: true,
+                                    })}
+                                </p>
+                                <p className="flex items-center text-gray-700 text-sm gap-2">
+                                    <strong className="text-gray-800">Travel Date:</strong>
+                                    {new Date(currentBooking?.flightDetails?.departureDate).toLocaleString("en-US", {
+                                        year: "numeric",
+                                        month: "long",
+                                        day: "numeric",
+                                        hour: "2-digit",
+                                        minute: "2-digit",
+                                        hour12: true,
+                                    })}
+                                </p>
+                                <p className="flex items-center text-gray-700 text-sm gap-2">
+                                    <strong className="text-gray-800">Total Price:</strong> ${currentBooking.totalPrice.toFixed(2)}
+                                </p>
+                                <p className="flex items-center text-sm font-medium gap-2">
+                                    <strong className="text-gray-800">Status:</strong>
+                                    <span
+                                        className={`px-3 py-1 rounded-full ${currentBooking.status === 'confirmed'
+                                                ? 'bg-green-100 text-green-700'
+                                                : 'bg-red-100 text-red-700'
+                                            }`}
+                                    >
+                                        {currentBooking.status}
+                                    </span>
+                                </p>
+                            </div>
                         </div>
                     ) : (
                         <p className="text-gray-600">No active bookings at the moment.</p>
                     )}
                 </div>
+
 
                 {/* Purchase History */}
                 <div>
@@ -160,7 +220,15 @@ export default function UserProfile() {
                                             <td className="px-4 py-2 text-gray-700 whitespace-nowrap">{history?.flightDetails?.destination}</td>
                                             <td className="px-4 py-2 text-gray-700 whitespace-nowrap">{history.numberOfSeats}</td>
                                             <td className="px-4 py-2 text-gray-700 whitespace-nowrap">
-                                                {new Date(history?.flightDetails?.departureDate).toLocaleDateString()}
+                                                {/* Format the departureDate to include both date and time */}
+                                                {new Date(history?.flightDetails?.departureDate).toLocaleString("en-US", {
+                                                    year: "numeric",
+                                                    month: "long",
+                                                    day: "numeric",
+                                                    hour: "2-digit",
+                                                    minute: "2-digit",
+                                                    hour12: true,
+                                                })}
                                             </td>
                                             <td className="px-4 py-2 text-gray-700 whitespace-nowrap">
                                                 {new Date(history.bookingDate).toLocaleDateString()}
